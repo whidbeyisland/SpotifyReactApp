3eb7b6901d9e5d5c981c2d108e39a4363b8107b0
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2021-12-11 21:26:01 -0800

    More sample scripts for testing

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const {spawn} = require('child_process');
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
- 
- var dataToSend;
+var largeDataSet = [];
  // spawn new child process to call the python script
- const python = spawn('python', ['script1.py']);
+ const python = spawn('python', ['script3.py']);
  // collect data from script
  python.stdout.on('data', function (data) {
   console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
+  largeDataSet.push(data);
  });
- // in close event we are sure that stream from child process is closed
+ // in close event we are sure that stream is from child process is closed
  python.on('close', (code) => {
  console.log(`child process close all stdio with code ${code}`);
  // send data to browser
- res.send(dataToSend)
+ res.send(largeDataSet.join(""))
  });
  
 })
