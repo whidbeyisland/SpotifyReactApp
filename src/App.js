17fda038197477b98e2b131d17b5b0c200401084
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2021-12-13 15:27:06 -0800

    Test React component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import hash from "./hash";
 import Player from "./Player";
 import logo from "./logo.svg";
 import "./App.css";
+import TestComponent from './TestComponent.js'
 
 class App extends Component {
   constructor() {
@@ -90,6 +91,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <TestComponent/>
+          <TestComponent/>
+
           <div id="test-div-1">
               <p id="test-p-1">Genres:</p>
           </div>
---
+++ src/TestComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TestComponent extends Component {
+
+  render() {
+    return(
+      <p>Click on an emoji to view the emoji short name.</p>
+    )
+  }
+
+}
