843e62d053766ec6ad8fd7ad0c9a0afcfbc5592e
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2022-03-11 18:39:26 -0800

    Adding min 5 songs to each genre array

--- client/src/GenreSongIds.js
+++ client/src/GenreSongIds.js
@@ -1,64 +1,119 @@
 export var genreSongIds = [
     // alternative rock
     [
-        [
-            'spotify:track:5UWwZ5lm5PKu6eKsHAGxOk', //Foo Fighters - Everlong
-            'spotify:track:6QewNVIDKdSl8Y3ycuHIei', //Pearl Jam - Even Flow
-            'spotify:track:6G0NzOx2jEPFsSmhr9N8Ys', //R.E.M. - Nightswimming
-        ]
+        'spotify:track:0HDaKOlVAfUWXdFR2RhBtN', //311 - Down
+        'spotify:track:0WHOPmOuQKuu05k4g3UTvk', //Everclear - Wonderful
+        'spotify:track:5UWwZ5lm5PKu6eKsHAGxOk', //Foo Fighters - Everlong
+        'spotify:track:6QewNVIDKdSl8Y3ycuHIei', //Pearl Jam - Even Flow
+        'spotify:track:6G0NzOx2jEPFsSmhr9N8Ys', //R.E.M. - Nightswimming
+        'spotify:track:2LuBKWDnJTryXmE1h1l54B', //Son Volt - Carnival Blues
+        'spotify:track:4wd09wCccmxUB7XVJp0RNn', //Wilco - Jesus, Etc.
+        ''
     ],
     // experimental hip hop
     [
-        [
-            'spotify:track:09rZgZRDbTpKwtACzo0teB', //Busdriver - Eat Rich
-            'spotify:track:5TFwGYhhFWa92FZXyIB7K8', //clipping. - Body & Blood
-            'spotify:track:7nCONy10IHp7XD3oYZ0lcx', //Death Grips - I've Seen Footage
-            'spotify:track:7satW8tFLasyZbftvrWFBP', //Run the Jewels - Legend Has It
-        ]
+        'spotify:track:09rZgZRDbTpKwtACzo0teB', //Busdriver - Eat Rich
+        'spotify:track:5TFwGYhhFWa92FZXyIB7K8', //clipping. - Body & Blood
+        'spotify:track:7nCONy10IHp7XD3oYZ0lcx', //Death Grips - I've Seen Footage
+        'spotify:track:6Mnf7IPJmv9gBh921JXioZ', //Flobots - Rise
+        'spotify:track:7fHqVBM914EJjzujOnUBFz', //Ho99o9 - Bone Collector
+        'spotify:track:7satW8tFLasyZbftvrWFBP', //Run the Jewels - Legend Has It
+        ''
     ],
     // indie poptimism
     [
-
+        'spotify:track:3TgMcrV32NUKjEG2ujn9eh', //The 1975 - It's Not Living (If It's Not with You)
+        'spotify:track:0kn0L1N40Ek2d1RYIuGicc', //EXES - Taxi
+        'spotify:track:3FpEXAupLwCHwzeUBxF99S', //joan - i loved you first
+        'spotify:track:4G82qlQup3mP62BURDdl35', //Mew - Hawaii
+        'spotify:track:6MZYWdxGka0BnG3KYO739d', //Patternist - I Don't Feel Real
+        ''
     ],
     // indie rock
     [
-        [
-            'spotify:track:2HgZBNeDPWBcIEFtqnYPgR', //The Dismemberment Plan - The Ice of Boston
-            'spotify:track:3jjsRKEsF42ccXf8kWR3nu', //Mitski - Washing Machine Heart
-            'spotify:track:3wzIilK62CXqB1mLyqvr4Z', //Weatherday - Porcelain Hands
-        ]
+        'spotify:track:2HgZBNeDPWBcIEFtqnYPgR', //The Dismemberment Plan - The Ice of Boston
+        'spotify:track:0ScgmigVOJr2mFsAtwFQmz', //Flaming Lips - Fight Test
+        'spotify:track:3jjsRKEsF42ccXf8kWR3nu', //Mitski - Washing Machine Heart
+        'spotify:track:5seoy3lNHpqtnBNnqdYoPF', //Modest Mouse - Florida
+        'spotify:track:03wKMRNYVvw6s9nm4I4jUS', //TV on the Radio - Wolf Like Me
+        'spotify:track:3wzIilK62CXqB1mLyqvr4Z', //Weatherday - Porcelain Hands
+        ''
     ],
     // indie soul
     [
-
+        'spotify:track:5Fw17rh1qEC6YoT32BijNI', //Janelle Monae - Take a Byte
+        'spotify:track:6Ko6VLnveSju6IZiwONLTt', //King Krule - Baby Blue
+        'spotify:track:7kC97zPE0PxrcItXyGdk8P', //Lianne la Havas - Paper Thin
+        'spotify:track:4pyR2RnK5A5vryIgQOcGis', //Sebastian Roka - You
+        'spotify:track:6GCIYIWUBSLontW6divqsw', //Solange - Stay Flo
+        ''
     ],
     // indietronica
     [
-
+        'spotify:track:0RUpgzacb8DwZB5B3IPO23', //Battles - Atlas
+        'spotify:track:6BNNl38VxGFvuRYwJbfWeh', //Fujiya & Miyagi - Transparent Things
+        'spotify:track:6kh5fj610DhNF1e02og1PL', //Grimes - Artangels
+        'spotify:track:5Po5YmT4RhawSeqNXx5m0o', //The Japanese House - Maybe You're the Reason
+        'spotify:track:3VhBuvzzuGUuh7nyJJvGLk', //LCD Soundsystem - Pow Pow
+        ''
     ],
     // new rave
     [
-
+        'spotify:track:3VHaFQF3O1nzJ2VOLN1ycJ', //Morgan Page feat. Tegan & Sara - Body Work
+        'spotify:track:048AQ5XBnvStuTn7X2pGSY', //Passion Pit - To Kingdom Come
+        'spotify:track:0ustKbONSPtdPBlmC1r0kP', //Shiny Toy Guns - Somewhere to Hide
+        'spotify:track:3mNHFC0fFqbTJKb2gxXE10', //Silversun Pickups - Cannibal
+        'spotify:track:4WLgP6qNrDQr5D6YQjoiwR', //Under the Influence of Giants - Ah Ha
+        ''
     ],
     // nu metal
     [
-
+        'spotify:track:4ClGNWLK9vZMBtO0CpnyOE', //Disturbed - Land of Confusion
+        'spotify:track:3UygY7qW2cvG9Llkay6i1i', //Evanescence - Going Under
+        'spotify:track:05NpeTQWnzXS1d8ZqL4YFZ', //Korn - Twisted Transistor
+        'spotify:track:2n1jBz26dVBtSbMvJNjGsu', //Linkin Park - Until It's Gone
+        'spotify:track:4e7yuouxvWPbK1q2kVlgz8', //Seether - Gasoline
+        'spotify:track:5en6cRkRnuE3U59QX6DAJh', //Trapt - Echo
+        ''
     ],
     // permanent wave
     [
-
+        'spotify:track:7CVG8gFuFM2OTkUVOkk4iX', //Armin van Buuren feat. Ne-Yo - Unlove You
+        'spotify:track:5mjsEhZjFFZeV9RSrCbQYT', //BT - Forget You [Michael Cassette Mix]
+        'spotify:track:7oK3Hn5fli8uCS1eiD3lBS', //Matt Fax - Close 2 U
+        'spotify:track:2wER3gVMMIWPusxOjoWlcQ', //Oliver Smith - Warehouse
+        'spotify:track:6aKtq5pTv4vgvr3X1ypR8U', //Rex Mundi - Bella Monaco
+        ''
     ],
     // pop
     [
-
+        'spotify:track:6tcLyhPrmBEPaQ1Yz4MdGy', //Ariana Grande - Greedy
+        'spotify:track:2Fxmhks0bxGSBdJ92vM42m', //Billie Eilish - bad guy
+        'spotify:track:6I9VzXrHxO9rA9A5euc8Ak', //Britney Spears - Toxic
+        'spotify:track:58pnv11Ku9Gak5OeeREw9C', //Doja Cat - Beautiful
+        'spotify:track:14WY9PjqIwABQF6BE386cz', //Gotye - Hearts a Mess
+        'spotify:track:6vt0I1cw1YmAIKDJvHVIM5', //Justin Timberlake feat. Jay-Z - Suit & Tie
+        'spotify:track:2kecdnlyueotEMC8rdNlf6', //Lady Gaga - Joanne
+        ''
     ],
     // punk
     [
-
+        'spotify:track:63ISXOWS5gmluVZabtaEzF', //Black Flag - Fix Me
+        'spotify:track:5szJ8SMwQVEvvdk65w6M6g', //Dead Kennedys - California Uber Alles
+        'spotify:track:7rSERmjAT38lC5QhJ8hnQc', //Dropkick Murphys - I'm Shipping Up to Boston
+        'spotify:track:1Dr1fXbc2IxaK1Mu8P8Khz', //Green Day - When I Come Around
+        'spotify:track:4S4X0TBauVoaCwElJ93Gl3', //The Pogues - Rainbow Men
+        'spotify:track:7Ig7b1MJJKbNyTxLU6u9rZ', //Stiff Little Fingers - Suspect Device
+        ''
     ],
     // soft rock
     [
-
+        'spotify:track:6eBK3edMW7bEzecF1eCezc', //Bryan Adams - (Everything I Do) I Do It for You
+        'spotify:track:1N0UflnZhn7xOkrN2l8zxx', //Hootie & the Blowfish - Time
+        'spotify:track:7j7LeOIVvfa3FB8m83unIU', //Howard Jones - Life in One Day
+        'spotify:track:5eU8qMd0TpaLqTGDZJaLDs', //INXS - Need You Tonight
+        'spotify:track:10tdqQKFhddpkjzxllTVRj', //Seal - Crazy
+        ''
     ],
 ];
 
